12b7ef6a30062525a1262930f4aecc1a7d19dc6d
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2014-05-23 20:26:44 -0300

    Clean up install script

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+# Script to install Kalibro Service and dependencies on Ubuntu 12.04
+# It may work on Debian 6 but this is untested
 
-#Bash unofficial strict mode: http://www.redsymbol.net/articles/unofficial-bash-strict-mode/
+# Bash unofficial strict mode: http://www.redsymbol.net/articles/unofficial-bash-strict-mode/
 set -eu
 set -o pipefail
+IFS=$'\n\t'
 
-#Set script configuration
-ANALIZO_VERSION='1.17.0'
+# Set script configuration
+ANALIZO_VERSION='1.17.0' # Version >1.17.0 needs Ubuntu 13.10/Debian 7
+KALIBRO_DOWNLOAD_URL='https://downloads.sourceforge.net/project/kalibrometrics/KalibroService.tar.gz'
 DATABASE_TYPE='POSTGRESQL'
 DATABASE_URL='jdbc:postgresql://localhost:5432'
 DATABASE_USER='kalibro'
@@ -13,38 +17,39 @@ DATABASE_PASSWORD='kalibro'
 WEBAPPS_DIR='/var/lib/tomcat6/webapps'
 TOMCAT_HOME='/usr/share/tomcat6'
 KALIBRO_TOMCAT_HOME="${TOMCAT_HOME}/.kalibro"
-KALIBRO_DIR="${KALIBRO_TOMCAT_HOME}"
 
-#Kalibro dependencies (including Analizo)-sudo touch /etc/apt/sources.list.d/analizo.list
+# Kalibro dependencies (including Analizo)
 sudo bash -c "echo \"deb http://analizo.org/download/ ./\" > /etc/apt/sources.list.d/analizo.list"
 sudo bash -c "echo \"deb-src http://analizo.org/download/ ./\" >> /etc/apt/sources.list.d/analizo.list"
 wget -O - http://analizo.org/download/signing-key.asc | sudo apt-key add -
 sudo apt-get update -qq
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y analizo=${ANALIZO_VERSION} tomcat6 tomcat6-common libtomcat6-java postgresql unzip
-
+# Add Kalibro database configuration to PostgresSQL
 sudo -u postgres psql --set ON_ERROR_STOP=1 < db_bootstrap.sql
 
-# Kalibro
-
+# Kalibro installation
+# Create temporary directory
 tmpdir=$(mktemp -d)
 trap 'rm -rf "${tmpdir}"' EXIT
 
-wget https://downloads.sourceforge.net/project/kalibrometrics/KalibroService.tar.gz \
--O "${tmpdir}/KalibroService.tar.gz"
-cp "${tmpdir}/KalibroService.tar.gz" "/var/tmp/KalibroService.tar.gz"
+# Download Kalibro
+wget "${KALIBRO_DOWNLOAD_URL}" -O "${tmpdir}/KalibroService.tar.gz"
 
+# Untar Kalibro
 tar -xzf "${tmpdir}/KalibroService.tar.gz" -C "${tmpdir}"
 
-sudo mkdir -p ${KALIBRO_DIR}
-
-for d in ${KALIBRO_DIR}/{projects,logs}; do
+# Create Kalibro directory structure on Tomcat dir
+sudo mkdir -p ${KALIBRO_TOMCAT_DIR}
+for d in ${KALIBRO_TOMCAT_DIR}/{projects,logs}; do
   ! [ -d "${d}" ] && sudo mkdir -p ${d}
 done
 
-sudo chown -R :tomcat6 ${KALIBRO_DIR}
-sudo chmod 'g+s,a+r,ug+w,o-w' -R ${KALIBRO_DIR}
+# Make tomcat6 user owner of Kalibro dir
+sudo chown -R :tomcat6 ${KALIBRO_TOMCAT_DIR}
+sudo chmod 'g+s,a+r,ug+w,o-w' -R ${KALIBRO_TOMCAT_DIR}
 
-echo | sudo tee ${KALIBRO_DIR}/kalibro.settings <<EOF
+# Add Kalibro Service settings to Tomcat
+echo | sudo tee ${KALIBRO_TOMCAT_DIR}/kalibro.settings <<EOF
 serviceSide: SERVER
 clientSettings:
   serviceAddress: "http://localhost:8080/KalibroService/"
@@ -56,8 +61,7 @@ serverSettings:
     username: "${DATABASE_USER}"
     password: "${DATABASE_PASSWORD}"
 EOF
-
-echo | sudo tee ${KALIBRO_DIR}/kalibro_test.settings <<EOF
+echo | sudo tee ${KALIBRO_TOMCAT_DIR}/kalibro_test.settings <<EOF
 serviceSide: SERVER
 clientSettings:
   serviceAddress: "http://localhost:8080/KalibroService/"
@@ -70,6 +74,7 @@ serverSettings:
     password: "${DATABASE_PASSWORD}"
 EOF
 
+# If Tomcat webapps dir doesn't exist, create it
 if [ ! -d "${WEBAPPS_DIR}" ]; then
   sudo mkdir ${WEBAPPS_DIR}
   sudo chown :tomcat6 ${WEBAPPS_DIR}
@@ -77,12 +82,8 @@ if [ ! -d "${WEBAPPS_DIR}" ]; then
   sudo chmod +x ${WEBAPPS_DIR}
 fi
 
-sudo cp "${tmpdir}/KalibroService/KalibroService.war" "${WEBAPPS_DIR}/"
+# Install Kalibro Service application on Tomcat
+sudo install -m=664 "${tmpdir}/KalibroService/KalibroService.war" "${WEBAPPS_DIR}/"
 
-#Imports sample configuration
-
-#CURRENT_DIR=$(pwd)
-#cd /var/lib/tomcat6/webapps/KalibroService/WEB-INF/lib
-#java -classpath "*" org.kalibro.ImportConfiguration ${CURRENT_DIR}/KalibroService/configs/Configuration.yml http://localhost:8080/KalibroService/
-
+# Restart Tomcat to start Kalibro Service
 sudo service tomcat6 restart
