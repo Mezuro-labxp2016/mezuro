f8d37180d1e9b963d811ba59a1f305eebe2111b0
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2014-05-23 15:28:39 -0300

    Fix and simplify script

--- install.sh
+++ install.sh
@@ -4,7 +4,16 @@
 set -eu
 set -o pipefail
 
+#Set script configuration
 ANALIZO_VERSION=1.17.0
+DATABASE_TYPE=POSTGRESQL
+DATABASE_URL="jdbc:postgresql://localhost:5432"
+DATABASE_USER=kalibro
+DATABASE_PASSWORD=kalibro
+WEBAPPS_DIR=/var/lib/tomcat6/webapps
+TOMCAT_HOME='/usr/share/tomcat6'
+KALIBRO_TOMCAT_HOME="${TOMCAT_HOME}/.kalibro"
+KALIBRO_DIR=${KALIBRO_TOMCAT_HOME}
 
 #Kalibro dependencies (including Analizo)-sudo touch /etc/apt/sources.list.d/analizo.list
 sudo bash -c "echo \"deb http://analizo.org/download/ ./\" > /etc/apt/sources.list.d/analizo.list"
@@ -17,50 +26,27 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y analizo=${ANALIZO_VERSION
 
 sudo -u postgres psql --set ON_ERROR_STOP=1 < db_bootstrap.sql
 
-
 # Kalibro
 
 tmpdir=$(mktemp -d)
 trap 'rm -rf "${tmpdir}"' EXIT
 
-if [ -f "/var/tmp/KalibroService.tar.gz" ]; then
-  cp "/var/tmp/KalibroService.tar.gz" "${tmpdir}/"
-else
-  wget https://downloads.sourceforge.net/project/kalibrometrics/KalibroService.tar.gz \
-      -O "${tmpdir}/KalibroService.tar.gz"
-  cp "${tmpdir}/KalibroService.tar.gz" "/var/tmp/KalibroService.tar.gz"
-fi
+wget https://downloads.sourceforge.net/project/kalibrometrics/KalibroService.tar.gz \
+-O "${tmpdir}/KalibroService.tar.gz"
+cp "${tmpdir}/KalibroService.tar.gz" "/var/tmp/KalibroService.tar.gz"
 
 tar -xzf "${tmpdir}/KalibroService.tar.gz" -C "${tmpdir}"
 
-TOMCAT_HOME=$(eval echo ~tomcat6)
-if ! [ -d "${TOMCAT_HOME}" ]; then
-  TOMCAT_HOME='/usr/share/tomcat6'
-  if ! [ -d "${TOMCAT_HOME}" ]; then
-    echo "Failed to determine tomcat6 home dir"
-    exit 1
-  fi
-fi
-
-KALIBRO_TOMCAT_HOME="${TOMCAT_HOME}/.kalibro"
-
-KALIBRO_DIR=${KALIBRO_TOMCAT_HOME}
-! [ -d ${KALIBRO_DIR} ] && sudo mkdir ${KALIBRO_DIR}
-KALIBRO_SUDO=sudo
+sudo mkdir -p ${KALIBRO_DIR}
 
 for d in ${KALIBRO_DIR}/{projects,logs}; do
-  ! [ -d "${d}" ] && ${KALIBRO_SUDO} mkdir -p ${d}
+  ! [ -d "${d}" ] && sudo mkdir -p ${d}
 done
 
-${KALIBRO_SUDO} chown -R :tomcat6 ${KALIBRO_DIR}
-${KALIBRO_SUDO} chmod 'g+s,a+r,ug+w,o-w' -R ${KALIBRO_DIR}
+sudo chown -R :tomcat6 ${KALIBRO_DIR}
+sudo chmod 'g+s,a+r,ug+w,o-w' -R ${KALIBRO_DIR}
 
-DATABASE_TYPE=POSTGRESQL
-DATABASE_URL="jdbc:postgresql://localhost:5432"
-DATABASE_USER=kalibro
-DATABASE_PASSWORD=kalibro
-
-echo | ${KALIBRO_SUDO} tee ${KALIBRO_DIR}/kalibro.settings <<EOF
+echo | sudo tee ${KALIBRO_DIR}/kalibro.settings <<EOF
 serviceSide: SERVER
 clientSettings:
   serviceAddress: "http://localhost:8080/KalibroService/"
@@ -73,7 +59,7 @@ serverSettings:
     password: "${DATABASE_PASSWORD}"
 EOF
 
-echo | ${KALIBRO_SUDO} tee ${KALIBRO_DIR}/kalibro_test.settings <<EOF
+echo | sudo tee ${KALIBRO_DIR}/kalibro_test.settings <<EOF
 serviceSide: SERVER
 clientSettings:
   serviceAddress: "http://localhost:8080/KalibroService/"
@@ -82,18 +68,14 @@ serverSettings:
   databaseSettings:
     databaseType: ${DATABASE_TYPE}
     jdbcUrl: "${DATABASE_URL}/kalibro_test"
-    username: "${DATABASE_TEST_USER}"
-    password: "${DATABASE_TEST_PASSWORD}"
+    username: "${DATABASE_USER}"
+    password: "${DATABASE_PASSWORD}"
 EOF
 
-WEBAPPS_DIR=/var/lib/tomcat6/webapps
-
-if [ ! -d "${WEBAPPS_DIR}" ]; then
-  sudo mkdir ${WEBAPPS_DIR}
-  sudo chown :tomcat6 ${WEBAPPS_DIR}
-  sudo chmod 664 -R ${WEBAPPS_DIR}
-  sudo chmod +x ${WEBAPPS_DIR}
-fi
+sudo mkdir ${WEBAPPS_DIR}
+sudo chown :tomcat6 ${WEBAPPS_DIR}
+sudo chmod 664 -R ${WEBAPPS_DIR}
+sudo chmod +x ${WEBAPPS_DIR}
 
 sudo cp "${tmpdir}/KalibroService/KalibroService.war" "${WEBAPPS_DIR}/"
 
