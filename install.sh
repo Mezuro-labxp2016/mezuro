2acb5c21813f1e901844d85970e39ca2f4bcbdbc
Daniel Miranda <danielkza2@gmail.com>
2015-07-20 15:08:53 -0300

    Use a cache directory for bundler if wanted

--- install.sh
+++ install.sh
@@ -26,22 +26,34 @@ pushd kalibro_processor
 psql -c "create role kalibro_processor with createdb login password 'kalibro_processor'" -U postgres
 cp config/database.yml.postgresql_sample config/database.yml
 cp config/repositories.yml.sample config/repositories.yml
+
+if [ -n "$CACHE_DIR" ]; then
+    mkdir -p "$CACHE_DIR/kalibro_processor/bundle"
+    export BUNDLE_PATH="$CACHE_DIR/kalibro_processor/bundle"
+fi
+
 export BUNDLE_GEMFILE=$PWD/Gemfile
 bundle install --deployment --without="test development" --retry=3
 RAILS_ENV=local bundle exec rake db:setup db:migrate
 RAILS_ENV=local bundle exec rails s -p 8082 -d
 RAILS_ENV=local bundle exec bin/delayed_job start
 popd
-unset BUNDLE_GEMFILE
+unset BUNDLE_GEMFILE BUNDLE_PATH
 
 # Kalibro Configurations
 git clone https://github.com/mezuro/kalibro_configurations.git -b v0.1.1 kalibro_configurations
 pushd kalibro_configurations
 psql -c "create role kalibro_configurations with createdb login password 'kalibro_configurations'" -U postgres
 cp config/database.yml.postgresql_sample config/database.yml
+
+if [ -n "$CACHE_DIR" ]; then
+    mkdir -p "$CACHE_DIR/kalibro_configurations/bundle"
+    export BUNDLE_PATH="$CACHE_DIR/kalibro_configurations/bundle"
+fi
+
 export BUNDLE_GEMFILE=$PWD/Gemfile
 bundle install --deployment --without="test development" --retry=3
 bundle exec rake db:setup db:migrate
 bundle exec rails s -p 8083 -d
 popd
-unset BUNDLE_GEMFILE
+unset BUNDLE_GEMFILE BUNDLE_PATH
