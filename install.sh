d2280b71de0f3f6974ef2d82cb7b8dfd9478287f
Daniel Miranda <danielkza2@gmail.com>
2015-07-20 13:27:55 -0300

    Make installation of Analizo optional

--- install.sh
+++ install.sh
@@ -6,14 +6,19 @@ set -o pipefail
 IFS=$'\n\t'
 
 # Set script configuration
-ANALIZO_VERSION='1.17.0' # Version >1.17.0 needs Ubuntu 13.10/Debian 7
+# Use default from environment if set, otherwise use 1.17.0 since newer versions need Ubuntu 13.10/Debian 7
+if [ -z "${ANALIZO_VERSION+x}" ]; then
+    ANALIZO_VERSION='1.17.0'
+fi
 
 # Kalibro dependencies (including Analizo)
-sudo bash -c "echo \"deb http://analizo.org/download/ ./\" > /etc/apt/sources.list.d/analizo.list"
-sudo bash -c "echo \"deb-src http://analizo.org/download/ ./\" >> /etc/apt/sources.list.d/analizo.list"
-wget -O - http://analizo.org/download/signing-key.asc | sudo apt-key add -
-sudo apt-get update -qq
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y analizo=${ANALIZO_VERSION} subversion git
+if [ -n "$ANALIZO_VERSION" ]; then
+    sudo bash -c "echo \"deb http://analizo.org/download/ ./\" > /etc/apt/sources.list.d/analizo.list"
+    sudo bash -c "echo \"deb-src http://analizo.org/download/ ./\" >> /etc/apt/sources.list.d/analizo.list"
+    wget -O - http://analizo.org/download/signing-key.asc | sudo apt-key add -
+    sudo apt-get update -qq
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y analizo=${ANALIZO_VERSION} subversion git
+fi
 
 # Kalibro Processor
 git clone https://github.com/mezuro/kalibro_processor.git -b v0.9.0 kalibro_processor
@@ -22,12 +27,12 @@ psql -c "create role kalibro_processor with createdb login password 'kalibro_pro
 cp config/database.yml.postgresql_sample config/database.yml
 cp config/repositories.yml.sample config/repositories.yml
 export BUNDLE_GEMFILE=$PWD/Gemfile
-bundle install --retry=3
+bundle install --deployment --without="test development" --retry=3
 RAILS_ENV=local bundle exec rake db:setup db:migrate
 RAILS_ENV=local bundle exec rails s -p 8082 -d
 RAILS_ENV=local bundle exec bin/delayed_job start
 popd
-export BUNDLE_GEMFILE=""
+unset BUNDLE_GEMFILE
 
 # Kalibro Configurations
 git clone https://github.com/mezuro/kalibro_configurations.git -b v0.1.1 kalibro_configurations
@@ -35,8 +40,8 @@ pushd kalibro_configurations
 psql -c "create role kalibro_configurations with createdb login password 'kalibro_configurations'" -U postgres
 cp config/database.yml.postgresql_sample config/database.yml
 export BUNDLE_GEMFILE=$PWD/Gemfile
-bundle install --retry=3
+bundle install --deployment --without="test development" --retry=3
 bundle exec rake db:setup db:migrate
 bundle exec rails s -p 8083 -d
 popd
-export BUNDLE_GEMFILE=""
+unset BUNDLE_GEMFILE
