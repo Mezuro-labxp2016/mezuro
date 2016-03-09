6acee5a3d4f405d106665b3603cbe8fc190403ad
Diego Araújo <diegoamc@protonmail.ch>
2016-03-09 16:05:31 -0300

    Implement first version of collector
    Signed-off-by: Eduardo Araújo <duduktamg@hotmail.com>

--- lib/kolekti/cc_phpmd/collector.rb
+++ lib/kolekti/cc_phpmd/collector.rb
@@ -1,5 +1,8 @@
 require 'kolekti/collector'
 
+require 'cc/cli'
+require 'cc/analyzer'
+
 module Kolekti
   module CcPhpMd
     class Collector < Kolekti::Collector
@@ -11,6 +14,20 @@ module Kolekti
         supported_metrics = parse_supported_metrics(
           File.expand_path("../metrics.yml", __FILE__), 'CodeClimate PHPMD', [:PHP])
         super('CodeClimate PHPMD', 'PHP Mess Detector', supported_metrics)
+      end
+
+      def collect_metrics(code_directory, wanted_metric_configurations)
+        engine_registry = CC::Analyzer::EngineRegistry.new
+        filesystem = CC::Analyzer::Filesystem.new('.')
+        formatter = CC::Analyzer::Formatters::JSONFormatter.new(filesystem)
+        config = CC::Yaml::Nodes::Root.new
+        config["engines"] = CC::Yaml::Nodes::EngineList.new(config).with_value({})
+        config.engines["phpmd"] = CC::Yaml::Nodes::Engine.new(config.engines).with_value("enabled" => true)
+
+        Dir.chdir(code_directory) do
+          runner = CC::Analyzer::EnginesRunner.new(engine_registry, formatter, code_directory, config, [])
+          runner.run
+        end
       end
     end
   end
