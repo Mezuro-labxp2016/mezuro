8e259880b06a37adcbdd6894bc6d49d3644d1dc1
Daniel Miranda <danielkza2@gmail.com>
2016-03-11 13:49:16 -0300

    Implement CodeClimate output parser

--- lib/kolekti/cc_phpmd/collector.rb
+++ lib/kolekti/cc_phpmd/collector.rb
@@ -1,5 +1,5 @@
+require 'kolekti/cc_phpmd/parser'
 require 'kolekti/collector'
-
 require 'cc/cli'
 require 'cc/analyzer'
 
@@ -10,24 +10,31 @@ module Kolekti
         system('codeclimate version', [:out, :err] => '/dev/null') ? true : false
       end
 
-      def initialize
-        supported_metrics = parse_supported_metrics(
-          File.expand_path("../metrics.yml", __FILE__), 'CodeClimate PHPMD', [:PHP])
-        super('CodeClimate PHPMD', 'PHP Mess Detector', supported_metrics)
-      end
+      def initialize   supported_metrics = parse_supported_metrics(
+      File.expand_path("../metrics.yml", __FILE__), 'CodeClimate PHPMD', [:PHP])
+      super('CodeClimate PHPMD', 'PHP Mess Detector', supported_metrics) end
 
-      def collect_metrics(code_directory, wanted_metric_configurations)
+      def collect_metrics(code_directory, wanted_metric_configurations, persistence_strategy)
         engine_registry = CC::Analyzer::EngineRegistry.new
         filesystem = CC::Analyzer::Filesystem.new('.')
-        formatter = CC::Analyzer::Formatters::JSONFormatter.new(filesystem)
+        parser = Kolekti::CcPhpMd::Parser.new(self, wanted_metric_configurations, persistence_strategy)
+
+        Dir.chdir(code_directory) do
+          runner = CC::Analyzer::EnginesRunner.new(engine_registry, parser, code_directory, config, [])
+          runner.run
+        end
+      end
+
+      def clean(code_directory, wanted_metric_configurations)
+      end
+
+      private
+
+      def config
         config = CC::Yaml::Nodes::Root.new
         config["engines"] = CC::Yaml::Nodes::EngineList.new(config).with_value({})
         config.engines["phpmd"] = CC::Yaml::Nodes::Engine.new(config.engines).with_value("enabled" => true)
-
-        Dir.chdir(code_directory) do
-          runner = CC::Analyzer::EnginesRunner.new(engine_registry, formatter, code_directory, config, [])
-          runner.run
-        end
+        config
       end
     end
   end
--- lib/kolekti/cc_phpmd/parser.rb
+++ lib/kolekti/cc_phpmd/parser.rb
@@ -1,6 +1,76 @@
+require 'json'
+require 'kolekti'
+require 'cc/analyzer/formatters/formatter'
+
 module Kolekti
   module CcPhpMd
-    class Parser
+    class Parser < CC::Analyzer::Formatters::Formatter
+      def initialize(collector, wanted_metric_configurations, persistence_strategy)
+        @collector = collector
+        @persistence_strategy = persistence_strategy
+        @wanted_metric_configurations = metric_configurations_by_name(wanted_metric_configurations)
+
+        @current_engine = nil
+      end
+
+      def write(_data)
+         begin
+           jsdata = JSON.parse(_data)
+         rescue JSON::ParserError
+           raise Kolekti::CollectorError("Failed parsing CodeClimate JSON data")
+         end
+
+         return if jsdata["type"] != "issue"
+
+         metric_configuration = @wanted_metric_configurations[jsdata["check_name"]]
+         return if metric_configuration.nil?
+
+         location = jsdata["location"]
+         path = location && location["path"]
+         lines = location && location["lines"]
+         line_number = lines && lines["begin"]
+         message = jsdata["description"]
+
+         raise Kolekti::CollectorError.new("Unexpected CodeClimate JSON data") if !path || !line_number || !message
+
+         @persistence_strategy.create_hotspot_metric_result(metric_configuration, module_name_for_path(path),
+                                                            line_number, message)
+      end
+
+      def started
+      end
+
+      def engine_running(engine)
+        yield
+      end
+
+      def finished
+      end
+
+      def close
+      end
+
+      def failed(_output)
+        raise Kolekti::CollectorError.new("CodeClimate runner failed: #{output}")
+      end
+
+      private
+
+      def metric_configurations_by_name(metric_configurations)
+        result = {}
+        metric_configurations.each do |code, metric_configuration|
+          metric = metric_configuration.metric
+          if metric.respond_to?(:metric_collector_name) && metric.metric_collector_name == @collector.name
+            result[metric.name] = metric_configuration
+          end
+        end
+        result
+      end
+
+      def module_name_for_path(file_path)
+        without_extension = file_path.rpartition('.').first
+        without_extension.gsub('.', '_').gsub('/', '.')
+      end
     end
   end
 end
