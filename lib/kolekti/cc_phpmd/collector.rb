b259094a8a0a957bd4dfc5171f13ef62dbf54f9a
Daniel Miranda <danielkza2@gmail.com>
2016-03-14 16:35:55 -0300

    Manually create the CodeClimate temp. dir. to avoid permission issues
    The CodeClimate temporary directory, that will hold the configuration
    JSON generated by the tool for the collector containers, has to be
    world-readable so the container can open it. To make sure of that,
    manually create it, properly setting up the umaks and permissions,
    instead of letting Docker do it, otherwise it will be owned by
    root:root.

--- lib/kolekti/cc_phpmd/collector.rb
+++ lib/kolekti/cc_phpmd/collector.rb
@@ -2,11 +2,30 @@ require 'kolekti/cc_phpmd/parser'
 require 'kolekti/collector'
 require 'cc/cli'
 require 'cc/analyzer'
+require 'fileutils'
 
 module Kolekti
   module CcPhpMd
     class Collector < Kolekti::Collector
+      def self.create_cc_dir
+        old_umask = File.umask
+        begin
+          File.umask(022)
+          FileUtils.mkdir_p('/tmp/cc', mode: 0755)
+        ensure
+          File.umask(old_umask)
+        end
+      end
+
       def self.available?
+        # Make sure the CodeClimate temporary directory is created world-readable,
+        # otherwise it may be possible for the container not to be able to properly
+        # access it.
+
+        # It has to be done before any CC invocation or Docker will create the directory
+        # owned by root (that behavior is fortunately deprecated starting with Docker 1.9)
+        create_cc_dir
+
         system('codeclimate version', [:out, :err] => '/dev/null') ? true : false
       end
 
@@ -15,6 +34,8 @@ module Kolekti
           File.expand_path("../metrics.yml", __FILE__),
           'CodeClimate PHPMD', [:PHP])
         super('CodeClimate PHPMD', 'PHP Mess Detector', supported_metrics)
+
+        self.class.create_cc_dir
       end
 
       def collect_metrics(code_directory, wanted_metric_configurations, persistence_strategy)
