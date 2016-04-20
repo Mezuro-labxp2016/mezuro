0e87f8f22cf018a4d6ba3096fab0ba4518271051
Rafael Reggiani Manzo <rr.manzo@protonmail.com>
2016-04-20 16:22:48 -0300

    Install simplecov
    It provides test coverage reports which is a fundamental input when
    working with TDD.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/spec/"
+  add_filter "/features/"
+
+  coverage_dir 'coverage/rspec'
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'kolekti/radon'
