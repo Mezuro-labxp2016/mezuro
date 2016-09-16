6be1ae549924e3a8c601746d6ab5f912bb54ced5
Athos Coimbra Ribeiro <ribeiro@fedoraproject.org>
2016-09-16 18:00:39 -0300

    install codeclimate

--- Vagrantfile
+++ Vagrantfile
@@ -6,5 +6,8 @@ Vagrant.configure(2) do |config|
     mezuro.vm.box = "debian/jessie64"
     mezuro.vm.network "private_network", ip: "10.18.0.115"
   end
+  config.vm.provider "libvirt" do |v|
+    v.memory = 2048
+  end
 
 end
---
+++ roles/prezento/files/install_phantomjs.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd /usr/local/share
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
+tar xjf phantomjs-1.9.7-linux-x86_64.tar.bz2
+sudo ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/share/phantomjs
+sudo ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs
+sudo ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/bin/phantomjs
