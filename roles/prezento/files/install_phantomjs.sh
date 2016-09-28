e05066280fd8c96b529ca1ead3e2f8eee071368d
Lucas Moura <lucas.moura128@gmail.com>
2016-09-28 17:04:16 -0300

    Fix symbolic link on phantomJS install.
    Signed-off-by: Lucas Moura <lucas.moura128@gmail.com>
    Signed-off-by: Athos Coimbra Ribeiro <ribeiro@fedoraproject.org>
    Signed-off-by: Gustavo Cayres <gustavocayrespublico@gmail.com>
    Signed-off-by: Herez Kattan <herezk@gmail.com>

--- roles/prezento/files/install_phantomjs.sh
+++ roles/prezento/files/install_phantomjs.sh
@@ -3,6 +3,6 @@
 cd /usr/local/share
 wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.7-linux-x86_64.tar.bz2
 tar xjf phantomjs-1.9.7-linux-x86_64.tar.bz2
-sudo ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/share/phantomjs
-sudo ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs
-sudo ln -s /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/bin/phantomjs
+sudo ln -fs /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/share/phantomjs
+sudo ln -fs /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/local/bin/phantomjs
+sudo ln -fs /usr/local/share/phantomjs-1.9.7-linux-x86_64/bin/phantomjs /usr/bin/phantomjs
